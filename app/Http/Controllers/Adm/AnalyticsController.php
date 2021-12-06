f93a3aaebef2b17972ddb33884b2883b48bc8000
Gabriel Moura <develop@srmoura.com.br>
2021-12-05 21:27:20 -0300

    Correção de bug no Analytics

--- app/Http/Controllers/Adm/AnalyticsController.php
+++ app/Http/Controllers/Adm/AnalyticsController.php
@@ -6,62 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\Benefits;
 use App\Models\Calendar;
 use App\Models\Clients;
-use App\Models\ClientStatus;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * Class AnalyticsController
+ * @package App\Http\Controllers\Adm
+ */
 class AnalyticsController extends Controller
 {
 
-    public function indexV2()
-    {
-        $statusM = collect();   //Precisa ser collect
 
-        $benefits = collect();    //Pode ser array
-        $month = Carbon::now()->subMonth()->month;
-        //https://hdtuto.com/article/laravel-eloquent-eager-load-count-relation-example
-
-        //  Quantidade de Clientes no mes
-        $statusM['client_count'] = Clients::whereMonth(
-            'created_at', '=', $month
-        )->where('company_id', '=', session()->get('company.id'))->count();
-
-
-        //  Multiplica valor pelo multiplicador e retorna o valor a receber no mes.
-        foreach (Clients::whereMonth('created_at', '=', $month)->where('company_id', '=', session()->get('company.id')) as $all) {
-            $statusM['amount_count'] = (float)$all->benefit()->first()->wage * $all->benefit()->first()->wage_factor;
-        }
-
-        //  Em Exigência/analise no ultimo mes
-        $statusM['requirement_count'] = ClientStatus::whereStatus('analysis')
-            ->whereStatus('requirement')
-            ->whereMonth('created_at', '=', $month)
-            ->count();
-
-        //  Deferidos do mes
-        $statusM['deferred_count'] = ClientStatus::whereStatus('deferred')
-            ->whereMonth('created_at', '=', $month)
-            ->count();
-        //Conta os agendamentos
-        $statusM['calendar_count'] = Calendar::whereMonth('created_at', '=', $month)->where('company_id', '=', session()->get('company.id'))
-            ->count();
-
-        //Novas Entradas no mes
-        /*      $statusM['new_entry']=Clients::whereStatus('analysis')->whereStatus('analysis')
-                  ->whereMonth('created_at', '=', $month)->count();
-      */
-        //  Conta Clientes por beneficios no ultimo mes
-        //foreach (Benefits::all() as $all) {
-        //  $benefits[$all['name']] = $all->clients()->count();
-        //}
-
-
-        $status = collect()->merge(['monthly' => $statusM->merge(['benefits' => $benefits])]);
-        dd($status);
-
-        return view('admin.analytics.index', compact('status'));
-    }
-
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         if (Auth::user()->hasRole('admin')) {
@@ -71,11 +29,14 @@ class AnalyticsController extends Controller
         }
     }
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function admin()
     {
-        $statusM = collect();   //Precisa ser collect
-        $statusN = collect();
-        $benefits = collect();    //Pode ser array
+        $statusM = [];
+        $status = collect();
+        $client_count = Clients::all()->count();
         $range = [
             'now' => Carbon::now()->month,
             'monthly' => Carbon::now()->subMonth()->month
@@ -83,49 +44,79 @@ class AnalyticsController extends Controller
 
         //https://hdtuto.com/article/laravel-eloquent-eager-load-count-relation-example
         foreach ($range as $index => $month) {
+            // Quantidade de Clientes no mes
+            $clients = Clients::whereMonth('created_at', '=', $month)->get();
+
+            // Quantidade total de Clientes
+            $statusM['client_count'] = $client_count;
 
 
-            //  Quantidade de Clientes no mes
-            $statusM['client_count'] = Clients::whereMonth(
-                'created_at', '=', $month
-            )->count();
-
-
-            //  Multiplica valor pelo multiplicador e retorna o valor a receber no mes.
-            foreach (Clients::whereMonth('created_at', '=', $month) as $all) {
-                $statusM['amount_count'] = (float)$all->benefit()->first()->wage * $all->benefit()->first()->wage_factor;
+            // Calcula valor total a receber
+            $statusM['amount_count'] = 0.00;
+            foreach ($clients as $all) {
+                $statusM['amount_count'] += calculateAmount($all->benefit()->first());
             }
 
-            //  Em Exigência/analise no ultimo mes
-            $statusM['requirement_count'] = ClientStatus::whereStatus('analysis')
-                ->whereStatus('requirement')
-                ->whereMonth('created_at', '=', $month)
-                ->count();
 
-            //  Deferidos do mes
-            $statusM['deferred_count'] = ClientStatus::whereStatus('deferred')
-                ->whereMonth('created_at', '=', $month)
-                ->count();
-            //Conta os agendamentos
+            // Deferidos do mes
+            $statusM['deferred_count'] = 0;
+            foreach ($clients as $all) {
+                $statusM['deferred_count'] += $all->status()->whereStatus('deferred')->count();
+            }
+            // Em Analise do mês
+            $statusM['analysis_count'] = 0;
+            foreach ($clients as $all) {
+                $statusM['analysis_count'] += $all->status()->whereStatus('analysis')->count();
+            }
+
+            $statusM['requirement_count'] = 0;
+            foreach ($clients as $all) {
+                $statusM['requirement_count'] += $all->status()->whereStatus('analysis')->whereStatus('requirement')->count();
+            }
+
+
+            // Conta os agendamentos do mês
             $statusM['calendar_count'] = Calendar::whereMonth('created_at', '=', $month)
                 ->count();
 
-            //Novas Entradas no mes
-            $statusM['new_entry'] = Clients::whereMonth('created_at', '=', $month)->count();
 
-            //  Conta Clientes por beneficios no ultimo mes
+            // Novas Entradas no mes
+            $statusM['new_entry'] = $clients->count();
+
+            // Conta Clientes por beneficios no mes
+            $statusM['benefits'] = [];
             foreach (Benefits::all() as $all) {
-
-                $benefits[$all['name']] = $all->clients()->count();
+                $count = 0;
+                foreach ($all->clients()->whereMonth('created_at', '=', $month)
+                             ->get() as $tt) {
+                    $count += 1;
+                }
+                $statusM['benefits'] = [$all['name'] => $count];
             }
-            $status[$index] = $statusM->merge(['benefits' => $benefits]);
+
+            // Conta Indicação
+            $countName = collect();
+            foreach ($clients as $client) {
+                if ($countName->has($client->recommendation()->first()->name)) {
+                    $countName[$client->recommendation()->first()->name] += 1;
+                } else {
+                    $countName[$client->recommendation()->first()->name] = 1;
+                }
+
+            }
+            $statusM['recommendations'] = $countName ?? [];
+
+            // Associa Calculos ao Mẽs
+            $status[$index] = $statusM;
         }
-        //$status = collect()->merge(['monthly' => $statusM->merge(['benefits' => $benefits])]);
-
-
-        return view('admin.analytics.index', compact('status'));
+        return view('admin.analytics.index', compact('status', 'range'));
     }
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function manager()
     {
         $statusM = [];
