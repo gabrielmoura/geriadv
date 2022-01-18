75935ef889935e148959db99c044d4d4f0f211a6
Gabriel DTI <dtimarcelom@gmail.com>
2022-01-18 17:06:14 -0300

    Corrige Provedor de Serviços

--- app/Providers/OtherStorageServiceProvider.php
+++ app/Providers/OtherStorageServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Storage;
+use League\Flysystem\Filesystem;
 class OtherStorageServiceProvider extends ServiceProvider
 {
   
