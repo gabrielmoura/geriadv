e507cb4994e16d09238e2f1fafe30d06daa44157
Gabriel DTI <dtimarcelom@gmail.com>
2022-04-12 16:24:51 -0300

    Corrige PathGenerator do MediaLibrary

--- app/Actions/MediaLibrary/PathGenerator.php
+++ app/Actions/MediaLibrary/PathGenerator.php
@@ -5,21 +5,21 @@ namespace App\Actions\MediaLibrary;
 
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator as BasePathGenerator;
-
+use App\Traits\CompanySessionTraits;
 /**
  * Class PathGenerator
  * @package App\Actions\MediaLibrary
  */
 class PathGenerator implements BasePathGenerator
 {
-
+    use CompanySessionTraits;
     /**
      * @param Media $media
      * @return string
      */
     public function getPath(Media $media): string
     {
-        return md5($media->id . config('app.key')) . '/';
+        return $this->blurName($media) . '/';
     }
 
 
@@ -29,7 +29,7 @@ class PathGenerator implements BasePathGenerator
      */
     public function getPathForConversions(Media $media): string
     {
-        return md5($media->id . config('app.key')) . '/conversions/';
+        return $this->blurName($media) . '/conversions/';
     }
 
     /**
@@ -38,6 +38,14 @@ class PathGenerator implements BasePathGenerator
      */
     public function getPathForResponsiveImages(Media $media): string
     {
-        return md5($media->id . config('app.key')) . '/responsive-images/';
+        return $this->blurName($media) . '/responsive-images/';
+    }
+    
+    protected function blurName($media): string 
+    { 
+        //return md5($media->id . config('app.key'));
+        
+        //  Este retorna um md5 do ID da Empresa.
+        return md5($this->getCompanyId()).DIRECTORY_SEPARATOR. $media->getKey();
     }
 }
