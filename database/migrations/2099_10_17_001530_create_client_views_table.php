755c220190a736d589f63eed2fc98f4d99da86bb
Gabriel Rodrigues de Moura <gabriel.all@yandex.com>
2022-04-23 15:59:48 -0300

    Corrige View
    * Remove dependencia de campos nullable.

--- database/migrations/2099_10_17_001530_create_client_views_table.php
+++ database/migrations/2099_10_17_001530_create_client_views_table.php
@@ -15,7 +15,7 @@ class CreateClientViewsTable extends Migration
         DB::statement("
          CREATE VIEW client_views AS
 
-         SELECT clients.id,
+          SELECT clients.id,
     clients.cpf,
     clients.last_name,
     clients.name,
@@ -33,26 +33,23 @@ class CreateClientViewsTable extends Migration
     clients.state,
     benefits.name AS benefit,
     client_statuses.status,
-    pendencies.cras AS pendency_cras,
-    pendencies.cpf AS pendency_cpf,
-    pendencies.rg AS pendency_rg,
-    pendencies.birth_certificate AS pendency_birth_certificate,
-    pendencies.proof_of_address AS pendency_proof_of_address,
-    pendencies.impossibility_to_sign AS pendency_impossibility_to_sign,
     notes.body AS note,
     recommendations.name AS recommendation,
     clients.tel1,
-    clients.slug
-   FROM (((((clients
-     JOIN benefits ON ((benefits.id = clients.benefit_id)))
-     JOIN client_statuses ON (((client_statuses.client_id = clients.id) AND (client_statuses.id = ( SELECT max(client_statuses_1.id) AS max
+    clients.slug,
+    pendencies.pendency,
+    companies.name AS company_name
+   FROM clients
+     JOIN benefits ON benefits.id = clients.benefit_id
+     LEFT JOIN client_statuses ON client_statuses.client_id = clients.id AND client_statuses.id = (( SELECT max(client_statuses_1.id) AS max
            FROM client_statuses client_statuses_1
-          WHERE (client_statuses_1.client_id = clients.id))))))
-     LEFT JOIN pendencies ON ((clients.pendency_id = pendencies.id)))
-     LEFT JOIN notes ON (((notes.client_id = clients.id) AND (notes.id = ( SELECT max(notes_1.id) AS max
+          WHERE client_statuses_1.client_id = clients.id))
+     LEFT JOIN pendencies ON clients.pendency_id = pendencies.id
+     LEFT JOIN notes ON notes.client_id = clients.id AND notes.id = (( SELECT max(notes_1.id) AS max
            FROM notes notes_1
-          WHERE (notes_1.client_id = clients.id))))))
-     JOIN recommendations ON ((clients.recommendation_id = recommendations.id)))
+          WHERE notes_1.client_id = clients.id))
+     LEFT JOIN recommendations ON clients.recommendation_id = recommendations.id
+     JOIN companies ON benefits.company_id = companies.id AND clients.company_id = companies.id
         ");
     }
 
